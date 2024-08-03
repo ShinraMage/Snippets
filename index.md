5cc3588d008c2cef5bb1922946b3ce886a6751a7
ShinraMage <shinra@posetmage.com>
2024-08-03 23:51:36 +0800

    move

---
+++ index.md
@@ -0,0 +1,12 @@
+---
+layout: home
+---
+
+<a href="{{ site.feed.path | default: 'feed.xml' | absolute_url }}">
+    <svg class="svg-icon orange">
+    <use xlink:href="{{ 'assets/minima-social-icons.svg#rss' | relative_url }}"></use>
+    </svg><span>{{ site.description }} Subscribe</span>
+</a>
+
+Sort by [Tags](./Tags)
+
---
+++ upload.ps1
@@ -0,0 +1,22 @@
+cd _includes
+./upload.bat
+cd ../
+
+cd _layouts
+./upload.ps1
+cd ../
+
+cd assets
+./upload.bat
+cd ../
+
+git config --local user.name "ShinraMage"
+git config --local user.email "shinra@posetmage.com"
+git remote set-url origin git@SH:ShinraMage/Snippets.git
+
+git submodule update --recursive --remote
+
+git pull
+git add .
+git commit -m "upload"
+git push
